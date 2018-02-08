913b5b0a304397640c83c23e7532a65158fd8689
Captain Parrot <fusa@kumail8.info>
2018-02-08 18:06:01 +0600

    Thamao

---
+++ js/shafi.js
@@ -0,0 +1,60 @@
+$('#carousel').owlCarousel({
+    loop:true,
+    margin:3,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    },
+    autoplay:true,
+
+    autoplayTimeout:1500
+})  
+
+$('#Carousel').owlCarousel({
+    loop:true,
+    margin:3,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    },
+    autoplay:true,
+
+    autoplayTimeout:1500
+})
+
+
+$('#myCarousel').owlCarousel({
+    loop:true,
+    margin:3,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    },
+    autoplay:true,
+
+    autoplayTimeout:2500
+})
